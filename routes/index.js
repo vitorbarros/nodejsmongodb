25be9472de9e19c6944daee900178539830ba13b
Vitor Barros <vitormonteirodebarros@gmail.com>
2016-08-28 23:11:27 -0300

    nod

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    var arr = [];
+    for(var i =0; i < 20; i++){
+        arr.push({
+            id: i,
+            name: 'Vitor ' + i
+        });
+    }
+    res.render('index', {
+        title: 'Express',
+        data: arr
+    });
 });
 
 module.exports = router;
